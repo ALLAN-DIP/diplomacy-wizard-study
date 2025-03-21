ed52422f1285d50bcc4abf7b0ba6818265e1c9b8
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-03-21 16:26:30 -0700

    edit : relative path editted.

--- rank_by_experts/rank_center/app/database.py
+++ rank_by_experts/rank_center/app/database.py
@@ -7,7 +7,7 @@ import datetime
 
 DB_NAME = ".db"
 DATABASE_URL = f"sqlite:///./{DB_NAME}"
-DATA_PATH = os.path.join(os.path.dirname(__file__), "../../../collected-data/n45")
+DATA_PATH = os.path.join(os.path.dirname(__file__), "../../../../collected-data/n45")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
