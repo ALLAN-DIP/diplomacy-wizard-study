ef1e7773acf2a4be4953afdccd7d2f1ff9bda9a1
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-07-26 18:47:29 -0700

    update : new treatment assignment based on scnario <-> treatment indepndence

--- treatments_assignment/treatments_scenarios.py
+++ treatments_assignment/treatments_scenarios.py
@@ -6,6 +6,12 @@ TYPES = ["null", "sugg-TB", "sugg-TKT", "sugg-VB", "sugg-VKT"]
 SIDE = ["null", "side-self", "side-other", "both"]
 FRICTION = ["null", "YCO", "ROO", "ERO"]
 
+SCENARIO_IDs = [1, 2, 3, 4]
+random.seed(0)
+random.shuffle(SCENARIO_IDs)
+TYPE2SCENARIO = dict(zip(TYPES[1:], SCENARIO_IDs))
+print("TYPE2SCENARIO:", TYPE2SCENARIO)
+
 # in total, there are 80 combinations
 # but not all combinations are valid
 
@@ -19,10 +25,12 @@ FRICTION = ["null", "YCO", "ROO", "ERO"]
 # also N-N-N is handled under delayed cases
 # therefor we have 40 combinations
 treatments = {
+    "scenario_id": [],
     "type": [],
     "side": [],
     "friction": []
 }
+
 for t, s, f in product(TYPES, SIDE, FRICTION):
     if t == "null":
         if s != "null" or f not in ["null", "ERO"]:
@@ -37,18 +45,22 @@ for t, s, f in product(TYPES, SIDE, FRICTION):
     s_str = "S" if s_str == "side-self" else s_str
     s_str = "O" if s_str == "side-other" else s_str
     f_str = "N" if f == "null" else f
-    print(f"{t_str}-{s_str}-{f_str}")
+    if t == "null":
+        print(f"{t_str}-{s_str}-{f_str}")
+    else:
+        print(f"SC{TYPE2SCENARIO[t]}-{t_str}-{s_str}-{f_str}")
     if t_str == "null" and s_str == "null" and f_str in ["null", "ERO"]:
         continue
     treatments["type"].append(t)
     treatments["side"].append(s)
     treatments["friction"].append(f)
+    treatments["scenario_id"].append(TYPE2SCENARIO[t] if t != "null" else 0)
 
 treatments_df = pd.DataFrame(treatments)
 treatments_df.to_csv("treatments.csv", index=False)
 print("length of treatments:", len(treatments_df))
 
-SCENARIO_IDs = [1, 2, 3, 4]
+
 participants_assignments = {
     "participant_id": [],
     "scenario1": [], # the scenario which appeared first
